7b40c1bdbe067683b94a47155e204d1e78a3f4c3
hw0k <hw0knam@gmail.com>
2020-03-26 20:17:39 +0900

    4-3 수정

--- chap4/4-3.kt
+++ chap4/4-3.kt
@@ -1,16 +1,16 @@
 fun main() {
-  println(max(1)(2))
-  println(max(4)(7))
-  println(max(3)(-1))
-  println(max(9)(9))
-  println(max(0)(8))
+  println(max(1)(2))   // expect: 2
+  println(max(4)(7))   // expect: 7
+  println(max(3)(-1))  // expect: 3
+  println(max(9)(9))   // expect: 9
+  println(max(0)(8))   // expect: 8
 }
 
-fun max(left: Int) = {
-  right: Int -> {
-    when {
-      left >= right -> left
-      else -> right
+val max = {
+  left: Int -> {
+    right: Int -> when {
+        left >= right -> left
+        else -> right
     }
   }
 }
